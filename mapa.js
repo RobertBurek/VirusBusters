366fd026c59778ad0186d3454a6bb3d166a16725
Robert Burek <robertburek@wp.pl>
2020-07-23 21:25:34 +0200

    Doprecyzowanie pozycji na mapie

--- mapa.js
+++ mapa.js
@@ -1,11 +1,8 @@
-var map = L.map('mapid').setView([52.2344339, 20.9342764], 11);
+var map = L.map('mapid').setView([52.18997, 21.0361], 11);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([52.2344339, 20.9342764]).addTo(map)
-    .bindPopup('Jakaś ulica 3<br> 00-950 Warszawa');
-    // .openPopup();
-
-// nPopup();
+L.marker([52.18777, 21.0161]).addTo(map)
+    .bindPopup('Domaniewska 9/11 <br> 02-663 Warszawa');
